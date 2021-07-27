4ac8cdd1741c602b76e2f689f0eb8796fbee0cb4
Harshit <bkharshitbk@gmail.com>
2021-07-28 03:15:11 +0530

    added social auth - google/facebook

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import firebase from 'firebase/app';
-import { Container, Grid, Panel, Row, Col, Button, Icon } from 'rsuite'
-import { auth } from '../misc/firebase'
+import { Container, Grid, Panel, Row, Col, Button, Icon, Alert } from 'rsuite'
+import { auth, database } from '../misc/firebase'
 
 export const SignIn = () => {
 
-    const signInWithProvider = (provider)=> {
-        auth.signInWithPopup(provider)
+    const signInWithProvider = async (provider)=> {
+
+        try {
+            const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+            if(additionalUserInfo.isNewUser){
+                await database.ref(`/profiles/${user.uid}`).set({
+                    name: user.displayName,
+                    createdAt: firebase.database.ServerValue.TIMESTAMP
+                })
+            }
+
+            Alert.success('Signed In', 4000);
+        } catch (err) {
+            Alert.error(err.message, 4000)
+        }
+
+        // console.log("result", result);
     }
 
     const onFacebookSignIn = ()=> {
@@ -31,11 +47,11 @@ export const SignIn = () => {
                             </div>
 
                             <div className="mt-3">
-                                <Button block color="blue">
+                                <Button block color="blue" onClick={onFacebookSignIn}>
                                     <Icon icon="facebook" /> Continue with Facebook
                                 </Button>
                                 
-                                <Button block color="green">
+                                <Button block color="green" onClick={onGoogleSignIn}>
                                     <Icon icon="google" /> Continue with Google
                                 </Button>
                             </div>
