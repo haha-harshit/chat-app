2b0a760b98a39e61554173cbf4adc952280a9e8c
Harshit <bkharshitbk@gmail.com>
2021-08-02 02:30:58 +0530

    started building side bar

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Dashboard = () => {
+    return (
+        <div>
+            HELLO I'M Dashboard
+        </div>
+    )
+
+}
+// import './components/dashboard/index.js';
+// export default Dashboard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Grid, Row, Col } from 'rsuite'
+import { SideBar } from '../components/SideBar'
 
 export const Home = () => {
     return (
-        <div>
-            HomePage
-        </div>
+        <Grid fluid className="h-100">
+            <Row>
+                <Col xs={24} md={8}>
+                    
+                    <SideBar />
+
+                </Col>
+            </Row>
+        </Grid>
     )
 }
