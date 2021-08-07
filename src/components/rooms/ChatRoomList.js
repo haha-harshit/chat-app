c9b752bf4586fa7b64971047eca673be9f6ebd53
Harshit <bkharshitbk@gmail.com>
2021-08-07 20:20:20 +0530

    make RoomList active by link

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Loader, Nav } from 'rsuite'
-import { useRooms } from '../../context/rooms.context'
-import { RoomItem } from './RoomItem'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Loader, Nav } from 'rsuite';
+import { useRooms } from '../../context/rooms.context';
+import { RoomItem } from './RoomItem';
 
 export const ChatRoomList = ({aboveElHeight}) => {
 
     const rooms = useRooms();
     // console.log('rooms: from ChatRoomList:',rooms);
+    const location = useLocation();
 
     return (
         <Nav
@@ -16,7 +18,8 @@ export const ChatRoomList = ({aboveElHeight}) => {
             className="overflow-y-scroll custom-scroll"
             style={{
                 height: `calc(100% - ${aboveElHeight}px)`
-            }} 
+            }}
+            activeKey={location.pathname}
         >
             {/* if no rooms */}
             {!rooms && (
@@ -25,7 +28,12 @@ export const ChatRoomList = ({aboveElHeight}) => {
 
             {rooms && rooms.length > 0 && rooms.map(room => 
                 (
-                    <Nav.Item key={room.id}>    
+                    <Nav.Item
+                        componentClass={Link}
+                        to={`/chat/${room.id}`}
+                        key={room.id}
+                        eventKey={`/chat/${room.id}`}
+                        >
                         <RoomItem room={room}/>
                     </Nav.Item> 
                 )
