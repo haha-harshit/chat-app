dd7ee777daae33332a14bd7b9a83da0ff7fc74f3
Harshit <bkharshitbk@gmail.com>
2021-08-10 14:48:31 +0530

    add chat desc modal

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { ButtonToolbar, Icon } from 'rsuite'
+import { Link } from 'react-router-dom';
 import { useCurrentRoom } from '../../../context/current-room.context'
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import { RoomInfoBtnModal } from './RoomInfoBtnModal';
 
 export const ChatTop = () => {
 
     const name = useCurrentRoom(v => v.name)
+    const isMobile = useMediaQuery('(max-width: 992px)');
 
     return (
         <div>
-            <h2 className="text-center">{name}</h2>
+            <div className = "d-flex justify-content-between align-items-center">
+                <h4>
+
+                    <Icon componentClass={Link} to="/" icon="arrow-circle-left" size="2x" className={isMobile ? "d-inline-block p-0 mr-2 text-blue link-unstyled" : "d-none"}/>
+                    <span className="text-disappear">
+                        {name}
+                    </span>
+
+                </h4>
+
+                <ButtonToolbar className="white-space: no wrap" >Todo</ButtonToolbar>
+            </div>
+
+            <div className="d-flex justify-content-between align-items-center">
+                <span>todo</span>
+                <RoomInfoBtnModal />
+            </div>
+
         </div>
     )
 };
